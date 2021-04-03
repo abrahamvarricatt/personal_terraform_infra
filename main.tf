82bdc19fba9e4b38ab5d191450210a68d4260b0f
Abraham V <abraham@blueapple.ca>
2021-04-03 16:15:15 -0400

    Adding ALB and related resources

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 locals {
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnets
+  public_subnet_ids  = module.vpc.public_subnets
+
   tags = merge(
     {
       "Name" = var.name
@@ -7,9 +11,17 @@ locals {
   )
 }
 
+data "aws_route53_zone" "this" {
+  count = var.create_route53_record ? 1 : 0
+
+  name         = var.route53_zone_name
+  private_zone = false
+}
+
 provider "aws" {
   region = var.region
 }
+
 
 ###################
 # VPC
@@ -29,6 +41,108 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
+
+  tags = local.tags
+}
+
+###################
+# Security groups
+###################
+module "alb_https_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/https-443"
+  version = "v3.17.0"
+
+  name        = "${var.name}-alb-https"
+  vpc_id      = local.vpc_id
+  description = "Security group with HTTPS ports open for specific IPv4 CIDR block (or everybody), egress ports are all world open"
+
+  ingress_cidr_blocks = sort(compact(var.alb_ingress_cidr_blocks)) #tfsec:ignore:AWS006
+
+  tags = merge(local.tags, var.alb_https_security_group_tags)
+}
+
+module "alb_http_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+  version = "v3.17.0"
+
+  name        = "${var.name}-alb-http"
+  vpc_id      = local.vpc_id
+  description = "Security group with HTTP ports open for specific IPv4 CIDR block (or everybody), egress ports are all world open"
+
+  ingress_cidr_blocks = sort(compact(var.alb_ingress_cidr_blocks)) #tfsec:ignore:AWS006
+
+  tags = merge(local.tags, var.alb_http_security_group_tags)
+}
+
+###################
+# ACM (SSL certificate)
+###################
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "v2.12.0"
+
+  create_certificate = true
+
+  domain_name = "" # join(".", [var.name, var.route53_zone_name])
+
+  zone_id = element(concat(data.aws_route53_zone.this.*.id, [""]), 0)
+
+  tags = local.tags
+}
+
+###################
+# ALB
+###################
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "v5.10.0"
+
+  name     = var.name
+  internal = var.internal
+
+  vpc_id          = local.vpc_id
+  subnets         = local.public_subnet_ids
+  security_groups = flatten([module.alb_https_sg.this_security_group_id, module.alb_http_sg.this_security_group_id, var.security_group_ids])
+
+  access_logs = {
+    enabled = var.alb_logging_enabled
+    bucket  = var.alb_log_bucket_name
+    prefix  = var.alb_log_location_prefix
+  }
+
+  listener_ssl_policy_default = var.alb_listener_ssl_policy_default
+  https_listeners = [
+    {
+      target_group_index = 0
+      port               = 443
+      protocol           = "HTTPS"
+      certificate_arn    = module.acm.this_acm_certificate_arn
+      action_type        = "forward"
+    },
+  ]
+
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      action_type = "redirect"
+      redirect = {
+        port        = 443
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    },
+  ]
+
+  target_groups = [
+    {
+      name                 = var.name
+      backend_protocol     = "HTTP"
+      backend_port         = var.atlantis_port
+      target_type          = "ip"
+      deregistration_delay = 10
+    },
+  ]
 
   tags = local.tags
 }
--- variables.tf
+++ variables.tf
@@ -39,3 +39,84 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "internal" {
+  description = "Whether the load balancer is internal or external"
+  type        = bool
+  default     = false
+}
+
+# ALB
+variable "alb_ingress_cidr_blocks" {
+  description = "List of IPv4 CIDR ranges to use on all ingress rules of the ALB."
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "alb_https_security_group_tags" {
+  description = "Additional tags to put on the https security group"
+  type        = map(string)
+  default     = {}
+}
+
+variable "alb_http_security_group_tags" {
+  description = "Additional tags to put on the http security group"
+  type        = map(string)
+  default     = {}
+}
+
+variable "security_group_ids" {
+  description = "List of one or more security groups to be added to the load balancer"
+  type        = list(string)
+  default     = []
+}
+
+variable "alb_log_bucket_name" {
+  description = "S3 bucket (externally created) for storing load balancer access logs. Required if alb_logging_enabled is true."
+  type        = string
+  default     = ""
+}
+
+variable "alb_log_location_prefix" {
+  description = "S3 prefix within the log_bucket_name under which logs are stored."
+  type        = string
+  default     = ""
+}
+
+variable "alb_logging_enabled" {
+  description = "Controls if the ALB will log requests to S3."
+  type        = bool
+  default     = false
+}
+
+variable "alb_listener_ssl_policy_default" {
+  description = "The security policy if using HTTPS externally on the load balancer. [See](https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html)."
+  type        = string
+  default     = "ELBSecurityPolicy-2016-08"
+}
+
+variable "atlantis_port" {
+  description = "Local port Atlantis should be running on. Default value is most likely fine."
+  type        = number
+  default     = 4141
+}
+
+
+# Route53
+variable "route53_zone_name" {
+  description = "Route53 zone name to create ACM certificate in and main A-record, without trailing dot"
+  type        = string
+  default     = ""
+}
+
+variable "route53_record_name" {
+  description = "Name of Route53 record to create ACM certificate in and main A-record. If null is specified, var.name is used instead. Provide empty string to point root domain name to ALB."
+  type        = string
+  default     = null
+}
+
+variable "create_route53_record" {
+  description = "Whether to create Route53 record for Atlantis"
+  type        = bool
+  default     = false
+}
